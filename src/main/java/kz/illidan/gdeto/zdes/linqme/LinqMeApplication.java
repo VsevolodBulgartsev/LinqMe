ab4760305dee29efcd941632ade4cf8c32dfa440
ruslan.babich <ruslan.babich@innoforce.kz>
2018-01-16 16:36:56 +0600

    first commit; create project from spring initializer: web

---
+++ src/main/java/kz/illidan/gdeto/zdes/linqme/LinqMeApplication.java
@@ -0,0 +1,12 @@
+package kz.illidan.gdeto.zdes.linqme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinqMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinqMeApplication.class, args);
+	}
+}
