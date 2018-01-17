ac2d827c6e97a570460ab83f06531ff328b4737d
vsevolod.bulgartsev <vsevolod.bulgartsev@allpay.kz>
2018-01-17 17:33:24 +0600

    master: edit Bet class add map variables and some shit, ofc

--- src/main/java/kz/illidan/gdeto/zdes/linqme/models/Bet.java
+++ src/main/java/kz/illidan/gdeto/zdes/linqme/models/Bet.java
@@ -1,6 +1,8 @@
 package kz.illidan.gdeto.zdes.linqme.models;
 
+import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * Created by vsevolod.bulgartsev on 17.01.2018.
@@ -8,9 +10,12 @@ import java.util.Map;
 public class Bet {
 
     private String userName;
-//    private Map<un, un>; //Первая ставка
-//    private Map<un, un>; // Вторая ставка
-
+    private Map<String, String> bet1 = new HashMap<String, String>(); //Первая ставка
+    private Map<String, String> bet2 = new HashMap<String, String>(); // Вторая ставка
+//    bet1.put("Иван","Дно");
+//    bet1.put("Рус","Ракал");
+//    bet2.put("Иван","Дотер");
+//    bet2.put("Рус","Морской житель");
 
     public String getUserName() {
         return userName;
@@ -19,4 +24,21 @@ public class Bet {
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
+    public Map<String, String> getBet1() {
+        return bet1;
+    }
+
+    public void setBet1(Map<String, String> bet1) {
+        this.bet1 = bet1;
+    }
+
+    public Map<String, String> getBet2() {
+        return bet2;
+    }
+
+    public void setBet2(Map<String, String> bet2) {
+        this.bet2 = bet2;
+    }
+
 }
